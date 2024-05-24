e2841e2a8087ece901776b454b7e00be3054c66a
amaan8429 <mohdamaan8429@gmail.com>
2024-05-24 19:20:41 +0530

    finished hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { IoDocumentTextOutline } from "react-icons/io5";
 
 const Hero = () => {
   return (
@@ -10,10 +12,45 @@ const Hero = () => {
           fill="white"
         />
         <Spotlight
-          className="top-10 left-full h-[80vh] w-[50vw]"
+          className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Welcome to my Portfolio
+          </p>
+
+          <TextGenerateEffect
+            words="I love building things for the web."
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Amaan, a Next.js Developer based in India.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Download my Resume"
+              icon={<IoDocumentTextOutline />}
+              position="right"
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
