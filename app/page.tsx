63f824715488778ca28d737b08b59e0c1ea564ee
amaan8429 <mohdamaan8429@gmail.com>
2024-05-25 00:08:27 +0530

    grid done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            { name: "Home", link: "/" },
+            { name: "About", link: "#about" },
+            { name: "Projects", link: "#projects" },
+            { name: "Contact", link: "#contact" },
+          ]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
