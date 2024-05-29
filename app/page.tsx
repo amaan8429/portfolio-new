e72910dbf6c11bd6acde5a09cb6729b8889bf472
amaan8429 <mohdamaan8429@gmail.com>
2024-05-29 21:47:37 +0530

    finished the project

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import { navItems } from "@/data";
 import MyProjects from "@/components/MyProjects";
 import TechStack from "@/components/TechStack";
 import Blogs from "@/components/Blogs";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -16,6 +18,8 @@ export default function Home() {
         <MyProjects />
         <TechStack />
         <Blogs />
+        {/* <Approach /> */}
+        <Footer />
       </div>
     </main>
   );
