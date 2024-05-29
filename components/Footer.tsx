ddcfd75e401a8155e65280e7f69fbea8bfb038e1
amaan8429 <mohdamaan8429@gmail.com>
2024-05-29 22:32:15 +0530

    footer bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pb-4 mb-[100px] md:mb-5" id="contact">
+    <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
